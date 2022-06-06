2636d8942cc1546acc90d4fb83858e0d7cf43c4d
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2022-06-06 13:13:09 -0300

    Error solucionado

--- WEB/WebDjango/Aplicacion/views.py
+++ WEB/WebDjango/Aplicacion/views.py
@@ -1,3 +1,14 @@
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return HttpResponse('vista inicio')
+def estaciones(request):
+    return HttpResponse('vista estaciones')
+def ciudadanos(request):
+    return HttpResponse('vista ciudadanos')
+def desechos(request):
+    return HttpResponse('vista desechos')
+def tickets(request):
+    return HttpResponse('vista tickets')
