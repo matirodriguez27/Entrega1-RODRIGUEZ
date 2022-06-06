5cf31e707e0b6ed8665130183389f272644b048a
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2022-06-06 19:04:24 -0300

    update

--- WEB/WebDjango/Aplicacion/urls.py
+++ WEB/WebDjango/Aplicacion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from Aplicacion import views
 
 urlpatterns = [
-    path('', views.inicio),
-    path('nuevoUsuario', views.nuevoUsuario),
+    path('', views.inicio, name = "inicio"),
+    path('nuevoUsuario', views.nuevoUsuario, name = "nuevoUsuario"),
 ]
